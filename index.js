ab1114b0ae4a5a136386ebda9cafe2b1bee87304
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-06-10 16:58:01 +0600

    final set in index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; 
 
 app.use(cors());
 
 const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('edusite learning Course API Running');
 })
 
-
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
+
 app.get('/course/:id', (req, res) => {
     const id = req.params.id;
     const course_details = courses.find(course => course.course_id === id);
@@ -24,6 +25,7 @@ app.get('/course/:id', (req, res) => {
         res.send(course_details);
     }
 })
+
 app.listen(port, () => {
     console.log(`edusite API Running In PORT : ${port}`);
 })
