78f30101e3399483d57632df975b5cdc45199048
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-06-10 16:54:32 +0600

    courses id set

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const course_details = courses.find(course => course.course_id === id);
+    if (course_details == null) {
+        res.send({ "error": "No Data" });
+    }
+    else {
+        res.send(course_details);
+    }
+})
 app.listen(port, () => {
     console.log(`edusite API Running In PORT : ${port}`);
 })
